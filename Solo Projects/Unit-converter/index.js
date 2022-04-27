94121724ad77f93edfa997e0a4a193dcf7edfc36
Gresham24 <tembogresham@gmail.com>
2022-04-27 15:24:21 +0200

    added unit converter solo project

---
+++ Solo Projects/Unit-converter/index.js
@@ -0,0 +1,21 @@
+let measurement = document.getElementById("metricUnit")
+
+// Meters to feet conversion
+let feet = measurement * 3.28084
+// Feet to meters conversion
+let meters = measurement * 0.3048
+
+// liters to gallons conversion
+let gallons = measurement * 0.264172
+// gallons to liters conversion
+let liters = measurement * 3.78541
+
+// kilograms to pounds conversion 
+let kilograms = measurement * 2.20462
+// pounds to kilograms conversion
+let pounds = measurement * 0.453592
+
+function metersToFeet() {
+    let feet = measurement * 3.28084
+    
+}
