291e4bfcb475edfa17f2ad4393c4ce1745652d9a
Gresham24 <tembogresham@gmail.com>
2022-05-03 00:27:12 +0200

    added comments with steps

--- Solo Projects/Password-generator/index.js
+++ Solo Projects/Password-generator/index.js
@@ -7,4 +7,13 @@ const lowerCase = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
 const specialCharacter = [ "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "~", "<", ">", "?", "[", "]", "{", "}"]
 const number = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-//
+// 1. create a function that will loop through each character array and grab a random character from each array
+
+// 2. the loop should: 
+//         - store each character into a string (the new password)
+//         - add another character to the password string until it's length is equal to the specified password length
+
+// 3. create a variable that will be used to store and display the password
+
+// 4. add the passwords inside the newPassword classes
+
