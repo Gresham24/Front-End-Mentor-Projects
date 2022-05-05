37e42852f816aab9a711543e916652f34fc96ce9
Gresham24 <tembogresham@gmail.com>
2022-05-05 19:28:36 +0200

    feedback from code reviews

--- Solo Projects/Unit-converter/index.js
+++ Solo Projects/Unit-converter/index.js
@@ -1,29 +1,24 @@
-const measurement = document.getElementById("metricUnit").textContent
-
-// Meters to feet conversion
-let feet = (measurement * 3.28084).toFixed(3)
-// Feet to meters conversion
-let meters = (measurement * 0.3048).toFixed(3)
-
-// liters to gallons conversion
-let gallons = (measurement * 0.264172).toFixed(3)
-// gallons to liters conversion
-let liters = (measurement * 3.78541).toFixed(3)
-
-// kilograms to pounds conversion 
-let kilograms = (measurement * 0.453592).toFixed(3)
-// pounds to kilograms conversion
-let pounds = (measurement * 2.20462).toFixed(3)
-
-// display Length strings
-let meterUnit = measurement + " meters = " + feet + " feet | " + measurement + " feet = " + meters + " meters" 
-let volumeUnit = measurement + " litres = " + gallons + " gallons | " + measurement + " gallons = " + liters + " litres" 
-let massUnit = measurement + " kilograms = " + pounds + " pounds | " + measurement + " pounds = " + kilograms + " kilograms" 
+const measurement = 20 
 
 function unitConversion() {
+// Conversions from Meters to feet, liters to gallons, kilograms to pounds and vice versa
+const feet = (measurement * 3.28084).toFixed(3)
+const meters = (measurement * 0.3048).toFixed(3)
+const gallons = (measurement * 0.264172).toFixed(3)
+const liters = (measurement * 3.78541).toFixed(3)
+const kilograms = (measurement * 0.453592).toFixed(3)
+const pounds = (measurement * 2.20462).toFixed(3)
+
+// display Length strings
+const meterUnit = `${measurement} meters = ${feet} feet | ${measurement} feet = ${meters} meters` 
+const volumeUnit = `${measurement} + litres = + ${gallons} + gallons | + ${measurement} + gallons = + ${liters} + litres` 
+const massUnit = `${measurement} + kilograms = + ${pounds} + pounds | + ${measurement} + pounds = + ${kilograms} + kilograms` 
+
     document.getElementById("lengthResult").textContent = meterUnit
     document.getElementById("volumeResult").textContent = volumeUnit
     document.getElementById("massResult").textContent = massUnit
+    document.getElementById("metricUnit").textContent = measurement
 }
 
 unitConversion()
+
