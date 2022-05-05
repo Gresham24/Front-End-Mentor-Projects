00a48d1b2fcbd46cf79f45b3677b01912da984a9
Gresham24 <tembogresham@gmail.com>
2022-05-05 22:13:22 +0200

    [PW-Generator] created new script with single array

--- Solo Projects/Password-generator/index.js
+++ Solo Projects/Password-generator/index.js
@@ -1,76 +1,53 @@
 // STEP BY STEP TO DO LIST:
-// create arrays made up of the characters to be used in the password generator
-// get the total number of characters in each array (the length) 
-// create the function that will store the for loops
-// create a variable assigned to a blank string - this will be used to store the generated password
-// create a for loop that will iterate through each array once (i < 1)
-// use the length (total number of elements in the array) as a range to generate a random number (random character position) stored in a variable
+// 1. create arrays made up of the characters to be used in the password generator
+// 2. get the total number of characters in each array (the length) 
+// 3. create an onclick event listener that will initialize a function that will store the for loops
+// 4. create a variable assigned to a blank string - this will be used to store the generated password
+// 5. create a variable that will generate and store the password class nodelist
+// 6. get the input value from the HTML input box 
+// 7. use the length (total number of elements in the array) as a range to generate a random number (random character position)
 //     - the random character position will be used as an index to get the random character from the array
-// access random array elements using the random character position as an index
-// reassign the blank string variable with a concatination of the radnom array elements
-// create a for loop with the previous loop inside of it to run as many times as the specified password length
-// add the password string inside the newPassword class
+// 8. access random array elements using the random character position as an index
+// 9. reassign the blank string variable with a concatination of the radnom array elements
+// 10. create a loop to iterates through the previous step as many times as the specified password length
+// 11. create a loop to iterate through the nodelist and input the newPassword string into each HTML element with the .password class
 
 
 // START OF SCRIPT
 
-// Declaration of global variables
 const buttonClick = document.getElementById("generatorBtn")
 
 //Declaration of array to choose characters from
-const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-const specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "~", "<", ">", "?", "[", "]", "{", "}"]
-const numChar = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+const fullCharacterArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "~", "<", ">", "?", "[", "]", "{", "}"]
 
-// Storing the length of each character set array
-const getUpperCaseLength = upperCase.length;
-const getLowerCaseLength = lowerCase.length;
-const getSpecialCharLength = specialCharacter.length;
-const getNumberLength = numChar.length;
+// Storing the array length 
+const getFullCharacterArrLength = fullCharacterArr.length;
 
-
-buttonClick.addEventListener('click', function generatorPassword(){
-    let newPassword1 = document.getElementById("password1")
-    let newPassword2 = document.getElementById("password2")
-    let newPassword3 = document.getElementById("password3")
-    let newPassword4 = document.getElementById("password4")
-
+buttonClick.addEventListener('click', function generatorPassword() {
+    // generates a nodelist of password classes
+    let newPassword = document.getElementsByClassName('password')
+    // gets the input value from HTML
     let getPasswordLength = document.getElementById("passwordLength").value
-    // TO DO: find a better name for arrPassword
-    let arrPassword = ''
-    for (let i = 0; i < 1; i++) {
-        // why does this work for any condition where i higher than any number above 1
-        for (let i = 0; i < getPasswordLength; i++) {
-            if (arrPassword.length < getPasswordLength) {
-                arrPassword += upperCase[Math.floor(Math.random() * getUpperCaseLength)]
-                if (arrPassword.length < getPasswordLength) {
-                    arrPassword += lowerCase[Math.floor(Math.random() * getLowerCaseLength)]
-                    if (arrPassword.length < getPasswordLength) {
-                        arrPassword += specialCharacter[Math.floor(Math.random() * getSpecialCharLength)]
-                        if (arrPassword.length < getPasswordLength) {
-                            arrPassword += numChar[Math.floor(Math.random() * getNumberLength)]
-                        }
-                    }
-                }
-            }
-        }
-        console.log(arrPassword) 
+    let passwordString = ''
+    for (let i = 0; i < getPasswordLength; i++) {
+        let getRandCharPosition = Math.floor(Math.random() * getFullCharacterArrLength)
+        passwordString = passwordString + fullCharacterArr[getRandCharPosition]
     }
-    newPassword1.innerText = arrPassword
-    newPassword2.innerText = arrPassword
-    newPassword3.innerText = arrPassword
-    newPassword4.innerText = arrPassword
-
+    newPassword[2].innerText = passwordString
 })
 
-// 
-// generatorPassword() 
-// generatorPassword()
-// generatorPassword()
-// generatorPassword()
-
-
-
-
 // END OF SCRIPT
+
+
+// for (let j = 0; j < newPassword.length; j++) {
+//     newPassword[j].innerText = passwordString
+// }
+
+// Scrimba suggested solution
+
+// function getRandomPassword() {
+//     let getRandCharPosition = Math.floor(Math.random() * getFullCharacterArrLength)
+//     passwordString = passwordString + fullCharacterArr[getRandCharPosition]
+// }
+
+// getRandomPassword()
