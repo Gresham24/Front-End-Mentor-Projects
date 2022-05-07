8c5155aa7e7d188d6fdeb59c22b5912f4d07e59a
Gresham24 <tembogresham@gmail.com>
2022-05-07 17:12:54 +0200

    [PW-Generator] cleaned up & added comments

--- Solo Projects/Password-generator/index.js
+++ Solo Projects/Password-generator/index.js
@@ -18,13 +18,16 @@
 const buttonClick = document.getElementById("generatorBtn")
 
 //Declaration of array to choose characters from
-const fullCharacterArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "~", "<", ">", "?", "[", "]", "{", "}"]
+const fullCharacterArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
+    "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
+    "r", "s", "t", "u", "v", "w", "x", "y", "z", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "!", "@", "#", "$", "%", "^", "&", "*", "(",
+    ")", "-", "_", "+", "=", "~", "<", ">", "?", "[", "]", "{", "}"]
 
 // Storing the array length 
 const getFullCharacterArrLength = fullCharacterArr.length;
 
+// function to create the random password by generating a random character from fullCharacterArr and concatinating it into an empty string 
 function generatorPassword() {
-
     let getPasswordLength = document.getElementById("passwordLength").value
     let passwordString = ''
     for (let i = 0; i < getPasswordLength; i++) {
@@ -33,23 +36,15 @@ function generatorPassword() {
     }
     return passwordString;
 }
+// click event to run a function that inputs the random password strinng into any element in the DOM with a class name of .password
+    buttonClick.addEventListener('click', function () {
+        let passwordBlocks = document.querySelectorAll('.password').length
+        let newPassword = document.getElementsByClassName('password')
+    
+        for (let j = 0; j < passwordBlocks; j++) {
+            newPassword[j].innerText = generatorPassword()
+        }
+    })
 
-buttonClick.addEventListener('click', function () {
-    let passwordBlocks = document.querySelectorAll('.password').length
-    let newPassword = document.getElementsByClassName('password')
-
-    for (let i = 0; i < passwordBlocks; i++) {
-        newPassword[i].innerText = generatorPassword()
-    }
-})
 
 // END OF SCRIPT
-
-// function randomNumber(getFullCharacterArrLength){
-//     return Math.floor(Math.random() * getFullCharacterArrLength)
-// }
-
-
-// possible solution to test: generate a random password for each element. 
-// 1. define a function which will return a new random password.
-// 2. call that function and assign the returned value to the blank element on each iteration
