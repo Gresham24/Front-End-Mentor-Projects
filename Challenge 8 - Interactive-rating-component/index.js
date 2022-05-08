c1a63f8c521209cdaa2db4a9e21a1f423e6a6c04
Gresham24 <tembogresham@gmail.com>
2022-05-08 19:26:13 +0200

    [Interactive R.C] added JS steps | changed btn tags

---
+++ Challenge 8 - Interactive-rating-component/index.js
@@ -0,0 +1,12 @@
+// STEPS:
+// 1. Fetch all the buttons within the rating wrapper div
+// 2. Loop through the buttons to see which button is clicked 
+// 3. Add an .active class to the button that is clicked
+// 4. Store the clicked button value (rating number) in a variable
+// 4. when the submit button is clicked:
+//      - replace the ratingResult string inside the .thankYouCard with "you selected [ratingX] out of 5"
+// 5. Display the .thankYouCard class
+// 6. Add an alert when the user clicks the submit button without selecting a rating 
+//      - you forgot to select a rating 
+
+
