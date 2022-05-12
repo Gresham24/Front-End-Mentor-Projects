98d2ef42260d49f8314549d62c14eeb60cf7e5f0
Gresham24 <tembogresham@gmail.com>
2022-05-13 01:27:14 +0200

    [PCSP] added JS variables + error text tag

---
+++ Challenge-9-ping-coming-soon-page/index.js
@@ -0,0 +1,9 @@
+const submit = document.getElementById('submitBtn');
+const email = document.getElementById('emailAddress').value;
+const error = document.getElementById('error-text').value;
+
+
+
+// submit.addEventListener('submit', function (event) {
+//     event.preventDefault()
+// })
