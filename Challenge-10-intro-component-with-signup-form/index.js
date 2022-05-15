0ed0e633d36b5ecb909518518e6d9b1b324d378d
Gresham24 <tembogresham@gmail.com>
2022-05-15 19:21:45 +0200

    [Intro Comp] JS - added variables

---
+++ Challenge-10-intro-component-with-signup-form/index.js
@@ -0,0 +1,13 @@
+
+
+const form = document.getElementById('signup-form')
+const fname = document.getElementById('fName').value
+const lname = document.getElementById('lName').value
+const emailInput = document.getElementById('emailAddress').value
+const passwordInput = document.getElementById('password').value
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+
+
+})
