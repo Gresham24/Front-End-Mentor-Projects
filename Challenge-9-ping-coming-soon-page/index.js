d4c61c9c66724e48b860f08972217e387ed4018d
Gresham24 <tembogresham@gmail.com>
2022-05-15 20:14:33 +0200

    [PCSP] JS - updated to do list

--- Challenge-9-ping-coming-soon-page/index.js
+++ Challenge-9-ping-coming-soon-page/index.js
@@ -1,3 +1,11 @@
+// TO DO LIST
+// 1. style the native error message
+// 2. declare variables for: button submit, email value, error text, valid email checker (regex)
+// 3. add an onsubmit event to check if the email is empty
+// 4. add if statement to check if the email matches the regex function
+// 5. display the error message whe criteria not met
+
+
 const submit = document.getElementById('submitBtn');
 const error = document.getElementById('error-text');
 const email = document.getElementById('email-input');
