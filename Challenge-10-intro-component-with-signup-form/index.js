162fae072f0bec7a340855821fddc0d484ffe8ac
Gresham24 <tembogresham@gmail.com>
2022-05-15 19:42:11 +0200

    [Intro Comp] JS - updated to do list

--- Challenge-10-intro-component-with-signup-form/index.js
+++ Challenge-10-intro-component-with-signup-form/index.js
@@ -1,4 +1,12 @@
-
+// TO DO:
+// 1. Create tags for error states
+// 2. Create styling for error state tags
+// 3. Store input values in variables
+// 4. Create a function to detect if the email field is a valid email using regex 
+// 5. Create an onsubmit event with a function to identify if each input field is not empty 
+//      - nest another if statement for the email input to detect if the email value matches the regex
+//      - add event.preventDefault() to restrict the form from submitting if none of the input field criterion are met
+// 6. Display error message and error state styling for input fields that are empty 
 
 const form = document.getElementById('signup-form')
 const fname = document.getElementById('fName').value
