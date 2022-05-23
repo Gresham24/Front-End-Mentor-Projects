b186b940f411e23da24bcba7ba7966809e276a14
Gresham24 <tembogresham@gmail.com>
2022-05-23 21:59:11 +0200

    [base apparel] completed JS section

---
+++ Challenge-11-base-apparel-coming-soon/index.js
@@ -0,0 +1,30 @@
+const getForm = document.getElementById('signup');    // get the form 
+
+const email = getForm.elements['Email'];    // get the email input by the name value
+
+const submit = document.getElementById('submitBtn');     // get the submit button
+
+const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;     // valid email format regex 
+
+const error = document.getElementById('error-text');    // error message to display
+
+// email validation function
+function validate() {
+    const emailValue = email.value;
+    if (emailRegex.test(emailValue)) {
+        alert('Submitted successfully!')
+        getForm.reset();
+        clearErrors();
+    } else {
+        error.innerHTML = 'Please provide a valid email';
+    }
+}
+
+submit.addEventListener('click', function (event) {
+    event.preventDefault();
+    validate();
+})
+// clears error messages
+function clearErrors() {
+    error.innerHTML = '';
+}
