f2bc0020c9134c907dae5ae713852b98bf39f6ad
Gresham24 <tembogresham@gmail.com>
2022-05-25 22:30:56 +0200

    [Intro Comp] implemented feedback

--- Challenge-10-intro-component-with-signup-form/index.js
+++ Challenge-10-intro-component-with-signup-form/index.js
@@ -1,21 +1,32 @@
+/*
+Notes: 
+I still need to refactor the code so that all the input fields are checked at once and each individual error message
+is displayed when it fails in the process. Currently all the logic works in isolation, which makes it difficult to add the success
+state on a successful submission - I had to create a separate if statement to check this.
+*/
+
+
 const getForm = document.getElementById('signupForm');
 
 // Regex for email format validation
 const emailFormat =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+// get input values by name element
+const fname = getForm.elements['Name'];
+const lname = getForm.elements['Surname'];
+const email = getForm.elements['emailAddress'];
+const password = getForm.elements['Password'];
+
+// get input field for all inputs to generate success state
+const inputField = document.getElementsByClassName('input-field');
+
 function formValidity() {
-    // get input values by name element
-    const fname = getForm.elements['Name'];
-    const lname = getForm.elements['Surname'];
-    const email = getForm.elements['emailAddress'];
-    const password = getForm.elements['Password'];
 
     // get trimmed values of inputs
     const fnameValue = fname.value.trim();
     const lnameValue = lname.value.trim();
     const emailValue = email.value.trim();
-    const passwordValue = password.value.trim();
 
     // get error state strings
     const nameErr = document.getElementById('fName-error');
@@ -26,44 +37,67 @@ function formValidity() {
     // adds an error message if the input is empty, if not, changes the border color and removes error message
     if (fnameValue === '') {
         nameErr.innerHTML = 'First name cannot be empty';
-        fname.className = 'error-state';
+        fname.classList.add('error-state');
     } else {
-        fname.className = 'success-state';
+        fname.classList.remove('error-state');
         nameErr.innerHTML = '';
     }
 
     if (lnameValue === '') {
         surnameErr.innerHTML = 'Last Name cannot be empty';
-        lname.className = 'error-state';
+        lname.classList.add('error-state');
     } else {
-        lname.className = 'success-state';
+        lname.classList.remove('error-state');
         surnameErr.innerHTML = '';
     }
 
     if (emailValue === '') {
         emailErr.innerHTML = 'Email cannot be empty';
-        email.className = 'error-state';
+        email.classList.add('error-state');
         // checks email format matches regex expression
     } else if (!emailValue.match(emailFormat)) {
         emailErr.innerHTML = 'Looks like this is not an email';
-        email.className = 'error-state';
+        email.classList.add('error-state');
+        email.classList.add('error-font');
     } else {
-        email.className = 'success-state';
+        email.classList.remove('error-state');
         emailErr.innerHTML = '';
     }
 
     if (password.value.length === 0) {
         passwordErr.innerHTML = 'Password cannot be empty';
-        password.className = 'error-state';
+        password.classList.add('error-state');
     } else {
-        password.className = 'success-state';
+        password.classList.remove('error-state');
         passwordErr.innerHTML = '';
     }
+// checks if all input values have been correctly entered at once
+    if (fnameValue !== '' &&
+        lnameValue !== '' &&
+        emailValue !== '' &&
+        emailValue.match(emailFormat) &&
+        password.value.length !== 0) {
+        // successState()
+        alert('Form submitted successfully!')
+        getForm.reset();
+    }
 }
+/* Function to add green boxes after submission:
+
+function successState() {
+    for (let i = 0; i < inputField.length; i++) {
+        inputField[i].classList.add('success-state');
+    }
+}
+
+*/
+
 
 getForm.addEventListener('submit', function (event) {
     event.preventDefault();
     formValidity();
+
+
 });
 
 // disables whitespace from being entered in the password field
