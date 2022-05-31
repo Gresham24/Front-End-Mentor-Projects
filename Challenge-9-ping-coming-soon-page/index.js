323963a1f26d4e54783fa1c0db2421d826fe8d60
Gresham24 <tembogresham@gmail.com>
2022-06-01 01:07:42 +0200

    [Ping coming soon] cleaned up code

--- Challenge-9-ping-coming-soon-page/index.js
+++ Challenge-9-ping-coming-soon-page/index.js
@@ -1,44 +1,24 @@
 const form = document.getElementById('form');
-const submit = document.getElementById('submitBtn');
-// const email = document.getElementById('email-input');
 const email = form.elements["email"];
 
 // Variables used to print the messages
-const error = document.querySelector('.error-text');
-const correctEmail = document.querySelector('.success-text');
-// const invalidEmail = document.querySelector('.error-text');
+const inputStatus = document.querySelector('.error-text');
 
 // email Regex
 const validEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 function validInput() {
-
     if (validEmail.test(email.value)) {
-        // alert('submitted successfully');
+        alert('Email address submitted!')
         clearErrors();
         form.reset();
     } else if (email.value === '') {
-        error.innerHTML = 'Please enter an email address';
-        email.classList.add('error-text');
+        inputStatus.innerHTML = 'Please enter an email address';
         email.classList.add('error-email-border');
     } else {
-        error.innerHTML = 'Please provide a valid email address';
+        inputStatus.innerHTML = 'Please provide a valid email address';
+        email.classList.add('error-email-border');
     }
-
-    // if (email.value === '') {
-    //     blankEmail.innerHTML = "Please enter an email address"
-    //     blankEmail.className = 'error-text'
-    //     email.id = 'error-email-border'
-        
-    // } else if (email.value.match(validEmail)) {
-    //     correctEmail.innerHTML = "Email submitted!"
-    //     correctEmail.className = 'success-text'
-
-    // } else {
-    //     invalidEmail.innerHTML = "Please provide a valid email address"
-    //     invalidEmail.className = 'error-text'
-    //     email.id = 'error-email-border'
-    // }
     return
 }
 form.addEventListener('submit', function (event) {
@@ -47,7 +27,8 @@ form.addEventListener('submit', function (event) {
 })
 
 function clearErrors() {
-    error.innerHTML = '';
     email.classList.remove('error-text');
     email.classList.remove('error-email-border');
+    inputStatus.classList.add('success-text');
+    inputStatus.innerHTML = ' ';
 }
