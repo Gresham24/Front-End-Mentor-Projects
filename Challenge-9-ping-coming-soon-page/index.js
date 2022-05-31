5ea4094584ffe1f8ed65f5c35c181df4f550b6d8
Gresham24 <tembogresham@gmail.com>
2022-05-31 23:59:09 +0200

    [Ping coming soon] refactored JS

--- Challenge-9-ping-coming-soon-page/index.js
+++ Challenge-9-ping-coming-soon-page/index.js
@@ -1,34 +1,53 @@
 const form = document.getElementById('form');
 const submit = document.getElementById('submitBtn');
-const email = document.getElementById('email-input');
+// const email = document.getElementById('email-input');
+const email = form.elements["email"];
 
 // Variables used to print the messages
-const blankEmail = document.querySelector('.error-text');
+const error = document.querySelector('.error-text');
 const correctEmail = document.querySelector('.success-text');
-const invalidEmail = document.querySelector('.error-text');
+// const invalidEmail = document.querySelector('.error-text');
 
-// Valid email format identifier
-const validEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+// email Regex
+const validEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 function validInput() {
-    if (email.value === '') {
-        blankEmail.innerHTML = "Please enter an email address"
-        blankEmail.className = 'error-text'
-        email.id = 'error-email-border'
-        
-    } else if (email.value.match(validEmail)) {
-        correctEmail.innerHTML = "Email submitted!"
-        correctEmail.className = 'success-text'
 
+    if (validEmail.test(email.value)) {
+        // alert('submitted successfully');
+        clearErrors();
+        form.reset();
+    } else if (email.value === '') {
+        error.innerHTML = 'Please enter an email address';
+        email.classList.add('error-text');
+        email.classList.add('error-email-border');
     } else {
-        invalidEmail.innerHTML = "Please provide a valid email address"
-        invalidEmail.className = 'error-text'
-        email.id = 'error-email-border'
+        error.innerHTML = 'Please provide a valid email address';
     }
+
+    // if (email.value === '') {
+    //     blankEmail.innerHTML = "Please enter an email address"
+    //     blankEmail.className = 'error-text'
+    //     email.id = 'error-email-border'
+        
+    // } else if (email.value.match(validEmail)) {
+    //     correctEmail.innerHTML = "Email submitted!"
+    //     correctEmail.className = 'success-text'
+
+    // } else {
+    //     invalidEmail.innerHTML = "Please provide a valid email address"
+    //     invalidEmail.className = 'error-text'
+    //     email.id = 'error-email-border'
+    // }
     return
 }
-
 form.addEventListener('submit', function (event) {
     event.preventDefault();
     validInput()
 })
+
+function clearErrors() {
+    error.innerHTML = '';
+    email.classList.remove('error-text');
+    email.classList.remove('error-email-border');
+}
