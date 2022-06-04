a896906cc3cecada199f517fd908157a34c1de0a
Gresham24 <tembogresham@gmail.com>
2022-06-04 21:07:07 +0200

    [FAQ Accordion] toggle back to normal state for accordion items

--- Challenge-12-faq-accordion/index.js
+++ Challenge-12-faq-accordion/index.js
@@ -11,63 +11,52 @@
 /*
 Look into: 
 - ToggleClass()
-- Aria 
-- Timeout delay at the end of a function
 - if .wrapper + .active
-- create a loop inside a function outside of the 
-*/ 
+- create a loop inside a function outside of the event listener
+*/
+
 
 
 const questionClick = document.getElementsByClassName('heading');
 const answerArr = document.getElementsByClassName('expanded-text');
 const arrowButton = document.getElementsByClassName('arrow-btn');
 
-
-
-// for (let i = 0; i < questionClick.length; i++) {
-//     let selectedItem = questionClick[i];
-//     selectedItem.addEventListener('click', function () {
-//         for (let j = 0; j < arrowButton.length; j++) {
-//             for (let k = 0; k < answerArr.length; k++) {
-//                 if (!selectedItem) {
-                
-//                 }
-//             }
-//         }// console.log(selectedItem);
-//     });
-// }
-
-
-// arrowButton[i].classList.add('clicked-arrow-btn');
-// answerArr[i].classList.add('show-text');
-// selectedItem.classList.add('selected-item');
-
-
-
+let i = null;
 
 for (let i = 0; i < questionClick.length; i++) {
+    questionClick[i].addEventListener('click', function () {
 
-
-    let selectedItem = questionClick[i];
-    selectedItem.addEventListener('click', function () {
-        if (selectedItem) {
-            for (let j = 0; j < arrowButton.length; j++) {      // iterates through each of the arrow buttons and flips the arrow button of the selected heading 
-                for (let k = 0; k < answerArr.length; k++) {    // iterates through the expanded text class list to display the selected text
-                    arrowButton[i].classList.add('clicked-arrow-btn');
-                    answerArr[i].classList.add('show-text');    // show the text by removing the overflow = hidden and adding max-height to the expanded text class
-                    selectedItem.classList.add('selected-item');
-                }
+        for (let j = 0; j < arrowButton.length; j++) {
+            for (let k = 0; k < answerArr.length; k++) {
+                arrowButton[i].classList.toggle('clicked-arrow-btn');
+                answerArr[i].classList.toggle('active');
+                questionClick[i].classList.toggle('active');
             }
-        }
-        console.log(selectedItem);
+        }// console.log(selectedItem);
     });
 }
 
-// const answer1 = document.getElementById('answer-one'),
-//     answer2 = document.getElementById('answer-two'),
-//     answer3 = document.getElementById('answer-three'),
-//     answer4 = document.getElementById('answer-four'),
-//     answer5 = document.getElementById('answer-five');
+
+
+
+
+// for (let i = 0; i < questionClick.length; i++) {
+
+
+//     let selectedItem = questionClick[i];
+//     selectedItem.addEventListener('click', function () {
+//         if (selectedItem) {
+//             for (let j = 0; j < arrowButton.length; j++) {      // iterates through each of the arrow buttons and flips the arrow button of the selected heading 
+//                 for (let k = 0; k < answerArr.length; k++) {    // iterates through the expanded text class list to display the selected text
+//                     arrowButton[i].classList.add('clicked-arrow-btn');
+//                     answerArr[i].classList.add('show-text');    // show the text by removing the overflow = hidden and adding max-height to the expanded text class
+//                     selectedItem.classList.add('selected-item');
+//                 }
+//             }
+//         }
+//         console.log(selectedItem);
+//     });
+// }
 
 /* 
     questionClick[i].addEventListener('click', function () {
