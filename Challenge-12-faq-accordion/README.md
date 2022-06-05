56a37ce7a645189c346e8d4c2b180e6f46a716ad
Gresham24 <tembogresham@gmail.com>
2022-06-05 23:04:27 +0200

    [FAQ Accordion] meeting feedback

--- Challenge-12-faq-accordion/README.md
+++ Challenge-12-faq-accordion/README.md
@@ -2,3 +2,5 @@
 This challenge was to build an FAQ Accordion page 
 
 ## Hosted at: 
+
+https://challenge-12-faq-accordion.netlify.app/
--- Challenge-12-faq-accordion/index.js
+++ Challenge-12-faq-accordion/index.js
@@ -3,15 +3,9 @@ const answerArr = document.getElementsByClassName('expanded-text');
 const arrowButton = document.getElementsByClassName('arrow-btn');
 
 for (let i = 0; i < questionClick.length; i++) {
-
     questionClick[i].addEventListener('click', function () {
-
-        for (let j = 0; j < arrowButton.length; j++) {
-            for (let k = 0; k < answerArr.length; k++) {
                 arrowButton[i].classList.toggle('clicked-arrow-btn');
                 answerArr[i].classList.toggle('active');
                 questionClick[i].classList.toggle('active');
-            }
-        }
     });
 }
