a2dbfa83d7d5da5ad328de5139a6e5175834d985
Gresham24 <tembogresham@gmail.com>
2022-06-16 19:11:19 +0200

    [invoice creator] removed items from HTML | added click event and display functions

--- Solo Projects/Invoice-Creator/index.js
+++ Solo Projects/Invoice-Creator/index.js
@@ -1,11 +1,4 @@
-/***************** 
 
-High-level summary to do list: 
-When a button is clicked, it should be added asa line item to the invoice and the total should update.
-Each item can only be added once. 
-When the send invoice button is clicked, the invoice (form) is submitted 
-
-*****************/
 
 // TO DO LIST:
 //      - when a specific button (invoice item) is clicked, fetch the price (value) attached to it
@@ -16,19 +9,92 @@ When the send invoice button is clicked, the invoice (form) is submitted
 //          - if it is, it should not be added
 //      - when the send invoice button is clicked, reset the invoice
 
-const washPrice = 10;
-const mowPrice = 20;
-const weedsPrice = 30;
 
-let total = null;
+/* ============================
+ Variables
+=============================== */
 
+// price calculation variables
+const washValue = 10;
+const mowValue = 20;
+const weedsValue = 30;
+
+const totalAmt = document.getElementById('total-amount');
+const emailBtn = document.getElementById('email-invoice');
+const addButtons = document.querySelectorAll('.item-button');
+
+// Get description items
+const wash = document.getElementById('wash');
+const mow = document.getElementById('mow');
+const weeds = document.getElementById('weeds');
+
+// Get item prices
+const washPrice = document.getElementById('wash-price');
+const mowPrice = document.getElementById('mow-price');
+const weedsPrice = document.getElementById('weeds-price');
+
+// Add invoice item buttons 
 const washBtn = document.getElementById('wash-btn');
 const mowBtn = document.getElementById('mow-btn');
 const weedsBtn = document.getElementById('weeds-btn');
 
+// Remove invoice item buttons
 const removeWash = document.getElementById('remove-wash');
 const removeMow = document.getElementById('remove-mow');
 const removeWeeds = document.getElementById('remove-weeds');
 
 
-const emailBtn = document.getElementById('email-invoice');
+
+
+/* ============================
+ GLOBAL FUNCTIONS
+=============================== */
+
+function addWashItem() {
+    wash.innerText = 'Wash Car';
+    removeWash.innerText = 'Remove';
+    washPrice.innerHTML = `<span class="currency">$</span>${washValue}`;
+}
+
+function addMowItem() {
+    mow.innerText = 'Mow Lawn';
+    removeMow.innerText = 'Remove';
+    mowPrice.innerHTML = `<span class="currency">$</span>${mowValue}`;
+}
+
+function addWeedsItem() {
+    weeds.innerText = 'Pull Weeds';
+    removeMow.innerText = 'Remove';
+    weedsPrice.innerHTML = `<span class="currency">$</span>${weedsValue}`;
+}
+
+function calcTotal() {
+    
+}
+
+
+/* ============================
+ EVENT LISTENERS
+=============================== */
+
+
+
+washBtn.addEventListener('click', function () {
+    addWashItem()
+    totalAmt.innerText = `$${washValue}`;
+})
+
+mowBtn.addEventListener('click', function () {
+    addMowItem()
+    totalAmt.innerText = `$${mowValue}`;
+})
+
+weedsBtn.addEventListener('click', function () {
+    addWeedsItem()
+    totalAmt.innerText = `$${weedsValue}`;
+})
+
+/* ============================
+ TEST CONSOLES
+=============================== */
+
