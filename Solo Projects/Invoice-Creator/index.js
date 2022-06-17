22412e858c16b0fd67e92293b8b2f3d3bf4689cb
Gresham24 <tembogresham@gmail.com>
2022-06-17 21:40:06 +0200

    [invoice creator] restructured invoice items HTML & Styling

--- Solo Projects/Invoice-Creator/index.js
+++ Solo Projects/Invoice-Creator/index.js
@@ -1,100 +1,78 @@
-
-
-// TO DO LIST:
-//      - when a specific button (invoice item) is clicked, fetch the price (value) attached to it
-//      - display the line item in the invoice [add it to an array]
-//      - add the price (value) to the total amount 
-//      - if the remove button is clicked, remove the line item from the array
-//      - check if a line item is already added/clicked/present in the array.
-//          - if it is, it should not be added
-//      - when the send invoice button is clicked, reset the invoice
-
+// Import stylesheets
+import './index.css';
 
 /* ============================
  Variables
 =============================== */
 
 // price calculation variables
-const washValue = 10;
-const mowValue = 20;
-const weedsValue = 30;
+// const washPrice = 10;
+// const mowPrice = 20;
+// const weedsPrice = 30;
+
+let total = '';
 
 const totalAmt = document.getElementById('total-amount');
-const emailBtn = document.getElementById('email-invoice');
+const resetBtn = document.getElementById('email-invoice');
 const addButtons = document.querySelectorAll('.item-button');
 
-// Get description items
-const wash = document.getElementById('wash');
-const mow = document.getElementById('mow');
-const weeds = document.getElementById('weeds');
-
-// Get item prices
-const washPrice = document.getElementById('wash-price');
-const mowPrice = document.getElementById('mow-price');
-const weedsPrice = document.getElementById('weeds-price');
-
-// Add invoice item buttons 
+// ADD invoice item
 const washBtn = document.getElementById('wash-btn');
 const mowBtn = document.getElementById('mow-btn');
-const weedsBtn = document.getElementById('weeds-btn');
+// const weedsBtn = document.getElementById('weeds-btn');
 
-// Remove invoice item buttons
+// REMOVE invoice item
 const removeWash = document.getElementById('remove-wash');
-const removeMow = document.getElementById('remove-mow');
-const removeWeeds = document.getElementById('remove-weeds');
+// const removeMow = document.getElementById('remove-mow');
+// const removeWeeds = document.getElementById('remove-weeds');
 
+let itemsDisplay = [];
 
-
+let invoiceItems = [
+    { desc: 'Wash Car', price: 10 },
+    { desc: 'Mow Lawn', price: 20 },
+    { desc: 'Pull Weeds', price: 30 },
+];
 
 /* ============================
  GLOBAL FUNCTIONS
 =============================== */
 
-function addWashItem() {
-    wash.innerText = 'Wash Car';
-    removeWash.innerText = 'Remove';
-    washPrice.innerHTML = `<span class="currency">$</span>${washValue}`;
-}
+for (let i = 0; i < addButtons.length; i++) {
+    for (let j = 0; j < invoiceItems.length; j++) {
+        if (addButtons[i].id && invoiceItems[j].id == washBtn.id) {
+            total = `$${invoiceItems[j].price}`
+            totalAmt.innerText = total;
 
-function addMowItem() {
-    mow.innerText = 'Mow Lawn';
-    removeMow.innerText = 'Remove';
-    mowPrice.innerHTML = `<span class="currency">$</span>${mowValue}`;
-}
 
-function addWeedsItem() {
-    weeds.innerText = 'Pull Weeds';
-    removeMow.innerText = 'Remove';
-    weedsPrice.innerHTML = `<span class="currency">$</span>${weedsValue}`;
+        }
+    }
 }
-
-function calcTotal() {
-    
-}
-
 
 /* ============================
  EVENT LISTENERS
 =============================== */
 
-
-
-washBtn.addEventListener('click', function () {
-    addWashItem()
-    totalAmt.innerText = `$${washValue}`;
-})
-
-mowBtn.addEventListener('click', function () {
-    addMowItem()
-    totalAmt.innerText = `$${mowValue}`;
-})
-
-weedsBtn.addEventListener('click', function () {
-    addWeedsItem()
-    totalAmt.innerText = `$${weedsValue}`;
-})
+washBtn.addEventListener('click', function (event) {
+    event.preventDefault;
+    totalAmt.innerText = `$${washPrice}`;
+});
 
 /* ============================
  TEST CONSOLES
 =============================== */
 
+// console.log(washBtn.id)
+
+/*
+get the object description:
+invoiceItems[i].desc
+
+get the object price:
+invoiceItems[i].price
+
+*/
+
+//   console.log(addButtons[i]);
+
+// console.log(invoiceItems[j]);
