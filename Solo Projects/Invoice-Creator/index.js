3b6295329e76b2db60e51069075922d889d53472
Gresham24 <tembogresham@gmail.com>
2022-06-18 11:21:55 +0200

    [invoice creator] added divs for line items

--- Solo Projects/Invoice-Creator/index.js
+++ Solo Projects/Invoice-Creator/index.js
@@ -1,78 +1,76 @@
-// Import stylesheets
-import './index.css';
 
 /* ============================
  Variables
 =============================== */
 
-// price calculation variables
-// const washPrice = 10;
-// const mowPrice = 20;
-// const weedsPrice = 30;
-
-let total = '';
 
 const totalAmt = document.getElementById('total-amount');
 const resetBtn = document.getElementById('email-invoice');
-const addButtons = document.querySelectorAll('.item-button');
-
+// Section to add items
+const lineItems = document.getElementById('list-of-items');
 // ADD invoice item
 const washBtn = document.getElementById('wash-btn');
 const mowBtn = document.getElementById('mow-btn');
-// const weedsBtn = document.getElementById('weeds-btn');
-
+const weedsBtn = document.getElementById('weeds-btn');
 // REMOVE invoice item
-const removeWash = document.getElementById('remove-wash');
+// const removeWash = document.getElementById('remove-wash');
 // const removeMow = document.getElementById('remove-mow');
 // const removeWeeds = document.getElementById('remove-weeds');
 
-let itemsDisplay = [];
-
 let invoiceItems = [
     { desc: 'Wash Car', price: 10 },
-    { desc: 'Mow Lawn', price: 20 },
-    { desc: 'Pull Weeds', price: 30 },
+    { service: mowBtn, desc: 'Mow Lawn', price: 20 }, // remove service btn
+    { service: weedsBtn, desc: 'Pull Weeds', price: 30 }, // remove service btn
 ];
+
+let service = '';
+let ServicesRequested = [];
+let finalTotal = 0;
+
 
 /* ============================
  GLOBAL FUNCTIONS
 =============================== */
 
-for (let i = 0; i < addButtons.length; i++) {
-    for (let j = 0; j < invoiceItems.length; j++) {
-        if (addButtons[i].id && invoiceItems[j].id == washBtn.id) {
-            total = `$${invoiceItems[j].price}`
-            totalAmt.innerText = total;
-
-
-        }
+function renderServices() {
+    let service = '';
+    for (let i = 0; i < invoiceItems.length; i++) {
+        service += `
+            <div class="line-item">
+                <p id="wash" class="item"> ${invoiceItems[i].desc}</p>
+		        <button id="remove-wash" class="remove-button">Remove</button>
+		        <p id="wash-price" class="price"><span class="currency">$</span> ${invoiceItems[i].price}</p>
+            </div>
+            `;
     }
+    lineItems.innerHTML = service;
 }
+
+// function getServices() {
+//     service = `
+//         <div class="line-item">
+//             <p id="wash" class="item"> ${invoiceItems[0].desc}</p>
+//     		<button id="remove-wash" class="remove-button">Remove</button>
+//     		<p id="wash-price" class="price"><span class="currency">$</span> ${invoiceItems[0].price}</p>
+//         </div>
+//             `;
+//     lineItems.innerHTML = service;
+// }
+
+
 
 /* ============================
  EVENT LISTENERS
 =============================== */
 
-washBtn.addEventListener('click', function (event) {
-    event.preventDefault;
-    totalAmt.innerText = `$${washPrice}`;
-});
+washBtn.addEventListener('click', function () {
+    ServicesRequested.push(renderServices())
+})
+
+
 
 /* ============================
  TEST CONSOLES
 =============================== */
 
-// console.log(washBtn.id)
-
-/*
-get the object description:
-invoiceItems[i].desc
-
-get the object price:
-invoiceItems[i].price
-
-*/
-
-//   console.log(addButtons[i]);
-
-// console.log(invoiceItems[j]);
+// renderServices() 
