df55f41b8baa0eda461fe0c5740dcebb098a844b
Gresham24 <tembogresham@gmail.com>
2022-06-19 13:45:32 +0200

    [invoice creator] added remove buttons | styled hover

--- Solo Projects/Invoice-Creator/index.js
+++ Solo Projects/Invoice-Creator/index.js
@@ -3,7 +3,6 @@
  Variables
 =============================== */
 
-
 const totalAmt = document.getElementById('total-amount');
 const resetBtn = document.getElementById('email-invoice');
 const lineItems = document.getElementById('list-of-items');
@@ -11,11 +10,8 @@ const lineItems = document.getElementById('list-of-items');
 const washBtn = document.getElementById('wash-btn');
 const mowBtn = document.getElementById('mow-btn');
 const weedsBtn = document.getElementById('weeds-btn');
-/******** Remove buttons ************/
-let selectedId = '';
 
 let servicesRequested = [];
-let removedLineItems = [];
 
 let availableItems = [
     { id: 'wash-car', desc: 'Wash Car', price: 10 },
@@ -29,19 +25,19 @@ let availableItems = [
  GLOBAL FUNCTIONS
 =============================== */
 
+
 /******** Generate invoice line items ************/
 function renderServices() {
     let service = '';
-    //  QST: Would it be more efficient to have the loop go through the servicesRequested array and the compare it to the objects in the availableItems array?
+// Would it be more efficient to have the loop go through the servicesRequested array and the compare it to the objects in the availableItems array? 
     for (let i = 0; i < servicesRequested.length; i++) {
         for (let j = 0; j < availableItems.length; j++) {
-            // How can I use the array method .includes() here instead of the comparison? I tried using the code on line 90 but it rendered things incrementally when clicked     
+// How can I use the array method .includes() here instead of the comparison?
             if (servicesRequested[i] == availableItems[j].desc) {
-                selectedId = availableItems[j].id;
                 service += `
                 <div class="line-item">
                     <p class="item"> ${availableItems[j].desc}</p>
-                    <button onclick="removeItem()" id="${availableItems[j].id}" class="remove-button">Remove</button>
+                    <button id="${availableItems[j].id}" class="remove-button">Remove</button>
                     <p class="price"><span class="currency">$</span> ${availableItems[j].price}</p>
                 </div>
                 `;
@@ -66,36 +62,16 @@ function calcTotal() {
 
 /******** Send/reset invoice ************/
 function resetInvoice() {
-    finalTotal = 0;
     totalAmt.innerText = '$0';
-    lineItems.innerText = '';
+    lineItems.innerText = null;
+    servicesRequested = [];
 }
 
-
-/******** Remove invoice items ************/
-
-function removeItem() {
-    let idList = '';
-    for (let i = 0; i < availableItems.length; i++) {
-        idList += availableItems[i].id+', ';
-    }
-    // removedLineItems.push(idList);
-
-    // if (selectedId == availableItems[i].id) {
-    //     console.log(availableItems[i].desc);
-    // }
-    console.log(idList);
-
-}
 
 
 /* ============================
  EVENT LISTENERS
 =============================== */
-
-
-
-
 
 
 /******** Add invoice items buttons ************/
@@ -104,6 +80,7 @@ washBtn.addEventListener('click', function (event) {
     servicesRequested.push('Wash Car');
     renderServices();
     calcTotal();
+// How else can prevent the buttons from adding items more than once apart from { once : true }? (right now i can't click them after removing items)
 }, { once: true })
 
 mowBtn.addEventListener('click', function (event) {
@@ -121,28 +98,34 @@ weedsBtn.addEventListener('click', function (event) {
 }, { once: true })
 
 /******** Remove buttons ************/
+document.body.addEventListener('click', function (event) {
+    if (event.target.id == 'wash-car') {
+        let itemIndex = servicesRequested.indexOf('Wash Car');
+        servicesRequested.splice(itemIndex, 1);
+        calcTotal();
+        renderServices();
+    }
+})
 
+document.body.addEventListener('click', function (event) {
+    if (event.target.id == 'mow-lawn') {
+        let itemIndex = servicesRequested.indexOf('Mow Lawn');
+        servicesRequested.splice(itemIndex, 1);
+        calcTotal();
+        renderServices();
+    }
+})
 
+document.body.addEventListener('click', function (event) {
+    if (event.target.id == 'pull-weeds') {
+        let itemIndex = servicesRequested.indexOf('Pull weeds');
+        servicesRequested.splice(itemIndex, 1);
+        calcTotal();
+        renderServices();
+    }
+})
 
 /******** Reset invoice button ************/
 resetBtn.addEventListener('click', function () {
     resetInvoice();
 })
-
-
-/* ============================
- Questions
-=============================== */
-
-// 1. What else can I use to prevent the buttons from adding items more than once apart from { once: true }?
-//      - After resetting the invoice, I can't add more any items   
-
-            // if (servicesRequested.includes(availableItems[i].desc)) {
-            //     service += `
-            //         <div class="line-item">
-            //             <p class="item"> ${availableItems[i].desc}</p>
-            //             <button class="remove-button">Remove</button>
-            //             <p class="price"><span class="currency">$</span> ${availableItems[i].price}</p>
-            //         </div>
-            //         `;
-            // }
