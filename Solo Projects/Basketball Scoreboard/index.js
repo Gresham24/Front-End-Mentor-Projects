851b921ea5c97b6b451ad41989e7b3e8d5184b6b
Gresham Tembo <70582296+Gresham24@users.noreply.github.com>
2022-07-25 22:31:57 +0200

    [bball score] renamed btn tags + js variables

---
+++ Solo Projects/Basketball Scoreboard/index.js
@@ -0,0 +1,9 @@
+/* ============================
+        VARIABLE DECLARATIONS
+=============================== */
+const homeScore = document.getElementById('home-score');
+const guestScore = document.getElementById('guest-score');
+
+// const allAddButtons = document.querySelectorAll('.add-btn');
+const resetButton = document.getElementById('reset-game');
+
