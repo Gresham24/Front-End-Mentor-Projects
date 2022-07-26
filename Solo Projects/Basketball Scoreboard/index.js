a0fb58acf516992acefb4049481873eaee1bd31f
Gresham Tembo <70582296+Gresham24@users.noreply.github.com>
2022-07-26 23:48:39 +0200

    [bball score] added score objs

--- Solo Projects/Basketball Scoreboard/index.js
+++ Solo Projects/Basketball Scoreboard/index.js
@@ -1,9 +1,48 @@
-/* ============================
-        VARIABLE DECLARATIONS
-=============================== */
+/* ======================================
+        VARIABLE & OBJECT DECLARATIONS
+========================================= */
 const homeScore = document.getElementById('home-score');
 const guestScore = document.getElementById('guest-score');
 
-// const allAddButtons = document.querySelectorAll('.add-btn');
+// get the home & guest scores and convert them to a number
+const TotalHomeScore = Number(homeScore.textContent)   
+const TotalGuestScore = Number(guestScore.textContent)
+
+// Get buttons
+const homeButtons = document.querySelectorAll('.home-button-wrapper');
+const guestButtons = document.querySelectorAll('.guest-button-wrapper');
 const resetButton = document.getElementById('reset-game');
 
+const hScoreObj = {
+        add1: 1,
+        add2: 2,
+        add3: 3
+}
+
+const qScoreObj = {
+        add1: 1,
+        add2: 2,
+        add3: 3
+}
+
+
+
+/* ======================================
+        GLOBAL FUNCTIONS
+========================================= */
+
+
+
+
+/* ============================
+        FUNCTIONS
+=============================== */
+
+// function addToHomeScore() {
+//         add1home.addEventListener('click', function (e) {
+//                 e.preventDefault;
+//                 console.log('test successful');
+//         })
+// }
+
+// addToHomeScore();
