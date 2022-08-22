64818a737137e88df69838e494ac472ca5052d1d
Gresham Tembo <70582296+Gresham24@users.noreply.github.com>
2022-08-22 23:26:39 +0200

    [Oldagram] removed commented html added forEach

--- Solo Projects/Oldagram/index.js
+++ Solo Projects/Oldagram/index.js
@@ -2,9 +2,9 @@
     VARIABLES
 =============================== */
 
-// const getPosts = document.getElementsByClassName('user-post');
 const likeBtn = document.getElementsByClassName('like-button');
 const btnWrapper = document.getElementsByClassName('post-button-wrapper');
+const userPosts = Array.from(document.getElementsByClassName('user-post'));
 
 const posts = [
     {
@@ -37,19 +37,14 @@ const posts = [
 ]
 
 
-const userPosts = Array.from(document.getElementsByClassName('user-post'));
-
-
 for (const { name, username, location, avatar, post, comment, likes } of posts) {
     renderPosts(avatar, name, location, post, likes, username, comment);
-
 }
 
 
 function renderPosts(avatar, name, location, post, likes, username, comment) {
-    for (const userPost of userPosts) {
-
-        userPost.innerHTML += `
+    userPosts.forEach(element => {
+        element.innerHTML += `
         <header>
             <img src="${avatar}" class="avatar-img" alt="Vincent van Gogh's profile photo">
             <div class="user-info">
@@ -72,38 +67,5 @@ function renderPosts(avatar, name, location, post, likes, username, comment) {
             <p class="username-caption"><span class="name">${username}</span> ${comment}</p>
         </article>
     `
-    }
+    });
 }
-
-
-
-
-// Loops through the classes that contain the posts to get the position
-// for (let i = 0; i < userPosts.length; i++) {
-    // let likeValue = posts[i].likes;
-    // const userPost = userPosts[i];
-    // Displays the posts on the DOM while fetching the user content from the posts array
-        // userPost.innerHTML = `
-        //     <header>
-        //         <img src="${posts[i].avatar}" class="avatar-img" alt="Vincent van Gogh's profile photo">
-        //         <div class="user-info">
-        //             <p class="username">${posts[i].name}</p>
-        //             <p class="location">${posts[i].location}</p>
-        //         </div>
-        //     </header>
-
-        //     <article>
-        //         <img src="${posts[i].post}" id="post-image-1" class="post-image" alt="post of Vincent van Gogh">
-        //     </article>
-
-        //     <article class="post-details">
-        //         <div class="post-button-wrapper">
-        //             <button class="like-button"><img src="images/icon-heart.png" alt="button to like post"></button>
-        //             <button><img src="images/icon-comment.png" alt="button to comment on the post"></button>
-        //             <button><img src="images/icon-dm.png" alt="button to share the post"></button>
-        //         </div>
-        //         <p id="likes-post-1" class="likes">${posts[i].likes} likes</p>
-        //         <p class="username-caption"><span class="name">${posts[i].username}</span> ${posts[i].comment}</p>
-        //     </article>
-        // `
-// }
