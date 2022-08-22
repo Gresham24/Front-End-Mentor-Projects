8395568db6f2c935e46f53c67c5367e53898abb0
Gresham Tembo <70582296+Gresham24@users.noreply.github.com>
2022-08-22 23:04:36 +0200

    [Oldagram] used forOf loops

--- Solo Projects/Oldagram/index.js
+++ Solo Projects/Oldagram/index.js
@@ -2,7 +2,7 @@
     VARIABLES
 =============================== */
 
-const userPosts = document.getElementsByClassName('user-post');
+// const getPosts = document.getElementsByClassName('user-post');
 const likeBtn = document.getElementsByClassName('like-button');
 const btnWrapper = document.getElementsByClassName('post-button-wrapper');
 
@@ -36,70 +36,74 @@ const posts = [
     }
 ]
 
-// Loops through the classes that contain the posts to get the position
-for (let i = 0; i < userPosts.length; i++) {
-    // let likeValue = posts[i].likes;
-    const userPost = userPosts[i];
-    // Displays the posts on the DOM while fetching the user content from the posts array
-    userPost.innerHTML = `
+
+const userPosts = Array.from(document.getElementsByClassName('user-post'));
+
+
+for (const { name, username, location, avatar, post, comment, likes } of posts) {
+    renderPosts(avatar, name, location, post, likes, username, comment);
+
+}
+
+
+function renderPosts(avatar, name, location, post, likes, username, comment) {
+    for (const userPost of userPosts) {
+
+        userPost.innerHTML += `
         <header>
-            <img src="${posts[i].avatar}" class="avatar-img" alt="Vincent van Gogh's profile photo">
+            <img src="${avatar}" class="avatar-img" alt="Vincent van Gogh's profile photo">
             <div class="user-info">
-                <p class="username">${posts[i].name}</p>
-                <p class="location">${posts[i].location}</p>
+                <p class="username">${name}</p>
+                <p class="location">${location}</p>
             </div>
         </header>
-
+    
         <article>
-            <img src="${posts[i].post}" id="post-image-1" class="post-image" alt="post of Vincent van Gogh">
+            <img src="${post}" id="post-image-1" class="post-image" alt="post of Vincent van Gogh">
         </article>
-
+    
         <article class="post-details">
             <div class="post-button-wrapper">
                 <button class="like-button"><img src="images/icon-heart.png" alt="button to like post"></button>
                 <button><img src="images/icon-comment.png" alt="button to comment on the post"></button>
                 <button><img src="images/icon-dm.png" alt="button to share the post"></button>
             </div>
-            <p id="likes-post-1" class="likes">${posts[i].likes} likes</p>
-            <p class="username-caption"><span class="name">${posts[i].username}</span> ${posts[i].comment}</p>
+            <p id="likes-post-1" class="likes">${likes} likes</p>
+            <p class="username-caption"><span class="name">${username}</span> ${comment}</p>
         </article>
     `
+    }
 }
 
 
 
 
-// Get the nodeList of like buttons
-for (let j = 0; j < btnWrapper.length; j++) {
+// Loops through the classes that contain the posts to get the position
+// for (let i = 0; i < userPosts.length; i++) {
+    // let likeValue = posts[i].likes;
+    // const userPost = userPosts[i];
+    // Displays the posts on the DOM while fetching the user content from the posts array
+        // userPost.innerHTML = `
+        //     <header>
+        //         <img src="${posts[i].avatar}" class="avatar-img" alt="Vincent van Gogh's profile photo">
+        //         <div class="user-info">
+        //             <p class="username">${posts[i].name}</p>
+        //             <p class="location">${posts[i].location}</p>
+        //         </div>
+        //     </header>
 
-    // console.log(btnWrapper[j].children);
+        //     <article>
+        //         <img src="${posts[i].post}" id="post-image-1" class="post-image" alt="post of Vincent van Gogh">
+        //     </article>
 
-    btnWrapper[j].addEventListener('click', event => {
-        
-        let target = event.target;
-        // console.log(btnWrapper[j].children[0]);
-        // console.log(likeBtn[0]);
-        // console.log(event.target);
-
-
-        // if (event.target.className === 'like-button') {
-        // }
-    });
-}
-
-
-
-
-
-
-
-
-/* ============================
-    FUNCTIONS
-=============================== */
-
-// function displayVincetLikes() {
-//     let vincentLikes = posts[0].likes;
-//     vincentLikes++;
-//     post1likeCount.innerText = `${vincentLikes} likes`;
+        //     <article class="post-details">
+        //         <div class="post-button-wrapper">
+        //             <button class="like-button"><img src="images/icon-heart.png" alt="button to like post"></button>
+        //             <button><img src="images/icon-comment.png" alt="button to comment on the post"></button>
+        //             <button><img src="images/icon-dm.png" alt="button to share the post"></button>
+        //         </div>
+        //         <p id="likes-post-1" class="likes">${posts[i].likes} likes</p>
+        //         <p class="username-caption"><span class="name">${posts[i].username}</span> ${posts[i].comment}</p>
+        //     </article>
+        // `
 // }
