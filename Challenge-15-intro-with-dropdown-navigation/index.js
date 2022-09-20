6632617e037215a0e93748c4b3ddc74ba7dc270c
Gresham Tembo <70582296+Gresham24@users.noreply.github.com>
2022-09-20 18:51:24 +0200

    [dropdown nav] desktop nav dropdown hover states

--- Challenge-15-intro-with-dropdown-navigation/index.js
+++ Challenge-15-intro-with-dropdown-navigation/index.js
@@ -19,34 +19,3 @@ function hideNavMenu() {
 
 showMenuButton.addEventListener('click', showNavMenu);
 closeMenuButton.addEventListener('click', hideNavMenu);
-
-
-/* ============================
-    DESKTOP INTERACTIVITY 
-=============================== */
-
-const dropdownButton = document.getElementsByClassName('menu-item');
-const companyButton = document.getElementById('company-btn');
-const featuresButton = document.getElementById('features-btn');
-
-
-
-Array.from(dropdownButton).forEach(item => {
-    // console.log(item);
-
-    item.addEventListener('click', buttonClickCheck);
-
-});
-
-// console.log(featuresButton.id);
-
-function buttonClickCheck() {
-    if (featuresButton.id == 'features-btn') {
-        featuresButton.classList.toggle('active');    
-        companyButton.classList.remove('active');
-    }
-    if (companyButton.id == 'company-btn') {
-        companyButton.classList.toggle('active');    
-        featuresButton.classList.remove('active');
-    }
-}
