633b2ec49f948b4d98d0bd0e4538d4bfe689a60d
Gresham Tembo <70582296+Gresham24@users.noreply.github.com>
2022-09-22 18:32:39 +0200

    [dropdown nav] mobile nav accordion

--- Challenge-15-intro-with-dropdown-navigation/index.js
+++ Challenge-15-intro-with-dropdown-navigation/index.js
@@ -19,3 +19,25 @@ function hideNavMenu() {
 
 showMenuButton.addEventListener('click', showNavMenu);
 closeMenuButton.addEventListener('click', hideNavMenu);
+
+
+// Nav accordion
+
+const featuresButton = document.getElementById('features-btn');
+const companyButton = document.getElementById('company-btn');
+const featuresLinks = document.getElementById('features-links');
+const companyLinks = document.getElementById('company-links');
+
+
+function toggleFeaturesBtn() {
+    console.log('clicked');
+    featuresLinks.classList.toggle('shown');
+}
+
+function toggleCompanyBtn() {
+    console.log('clicked');
+    companyLinks.classList.toggle('shown');
+}
+
+featuresButton.addEventListener('click', toggleFeaturesBtn);
+companyButton.addEventListener('click', toggleCompanyBtn);
