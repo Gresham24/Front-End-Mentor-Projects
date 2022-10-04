1ad36a9966d114b4d6b1b08d8881c1ee5bfa30dd
Gresham Tembo <70582296+Gresham24@users.noreply.github.com>
2022-10-04 23:59:30 +0200

    [Tindog] button variables

--- Solo Projects/Tindog/index.js
+++ Solo Projects/Tindog/index.js
@@ -1,2 +1,5 @@
 // Remember to import the data and Dog class!
 import { dogs } from './data.js';
+
+const likeButton = document.getElementById('accept-btn')
+const dislikeButton = document.getElementById('decline-btn')
