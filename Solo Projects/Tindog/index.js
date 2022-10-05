30f9042d38a15632105724f6ae95014f9cacb122
Gresham Tembo <70582296+Gresham24@users.noreply.github.com>
2022-10-05 23:59:28 +0200

    [Tindog] added to do list

--- Solo Projects/Tindog/index.js
+++ Solo Projects/Tindog/index.js
@@ -1,5 +1,17 @@
-// Remember to import the data and Dog class!
+/*
+To do  list:
+
+1. When  like button is clicked, display like badge for 1sec
+2. change status to swiped true
+3. when dislike button is clicked, display the nope badge for 1s
+4. 
+
+
+*/ 
+
+
 import { dogs } from './data.js';
 
 const likeButton = document.getElementById('accept-btn');
 const dislikeButton = document.getElementById('decline-btn');
+
