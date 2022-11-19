a5060ccd7d9b2ad737a3437403dea5b507e9b053
Gresham Tembo <70582296+Gresham24@users.noreply.github.com>
2022-11-19 21:24:40 +0200

    [Adv. Gen] API call to fetch and display advice

---
+++ Challenge-16-advice-generator/index.js
@@ -0,0 +1,9 @@
+fetch('https://api.adviceslip.com/advice')
+    .then(res => res.json())
+    .then(data => {
+        const adviceID = data.slip.id;
+        const adviceText = data.slip.advice;
+        document.getElementById('advice-id').textContent = `Advice #${data.slip.id}`;
+        document.getElementById('advice-text').textContent = data.slip.advice;
+    });
+
