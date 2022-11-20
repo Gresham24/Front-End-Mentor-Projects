e5c37a013b6ddcad55e1f13a385433cdb518a6f4
Gresham Tembo <70582296+Gresham24@users.noreply.github.com>
2022-11-20 15:39:20 +0200

    [adv. gen] deferred script & removed const

--- Challenge-16-advice-generator/index.js
+++ Challenge-16-advice-generator/index.js
@@ -4,8 +4,6 @@ adviceGeneratorBtn.addEventListener('click', function () {
     fetch('https://api.adviceslip.com/advice')
     .then(res => res.json())
     .then(data => {
-        const adviceID = data.slip.id;
-        const adviceText = data.slip.advice;
         document.getElementById('advice-id').textContent = `Advice #${data.slip.id}`;
         document.getElementById('advice-text').textContent = data.slip.advice;
     });
